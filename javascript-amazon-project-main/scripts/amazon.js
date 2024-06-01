ee0445d3becdf35642b60fc827a18b4f3bfa46d8
MarySnik <mary.snikk@gmail.com>
2024-06-01 09:48:15 +0400

    new

---
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -0,0 +1,26 @@
+const products = [{
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating: {
+    stars: 4.5,
+    count: 87.
+  },
+  priceCents: 1090,
+}, {
+  image:'image/products/intermediate-composite-basketball.jpg',
+  name: 'Intermediate Size Basketball',
+  rating: {
+    stars: 4,
+    count: 127,
+  },
+  priceCents:2095,
+},
+{
+  image:'image/products/adults-plain-cotton-tshirt-2-pacl-teal.jpg',
+  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+  rating: {
+    stars:4.5,
+    count:56,
+  },
+  priceCents:799,
+}];
